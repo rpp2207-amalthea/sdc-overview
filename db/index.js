f2013e7e4a2e84f3e6c0c0ab69261f4191c4c014
Tony Vo <vo.tony12@gmail.com>
2023-01-14 13:46:06 -0700

    successful psql connection and inistializing first table

--- db/index.js
+++ db/index.js
@@ -1,1 +1,42 @@
-// MONGO DB Index
+const { Client } = require('pg');
+
+const client = new Client({
+  host: '127.0.0.1',
+  user: 'tivo',
+  database: 'sdc',
+  password: '',
+  port: 5432,
+});
+
+const execute = async (query) => {
+  try {
+    await client.connect();
+    await client.query(query);
+    return true;
+  } catch (error) {
+    console.error(error.stack);
+    return false;
+  } finally {
+    await client.end();
+  }
+}
+
+const text = `
+    CREATE TABLE IF NOT EXISTS product (
+      product_id INTEGER,
+      campus VARCHAR(10),
+      name VARCHAR(50),
+      slogan VARCHAR(100),
+      description VARCHAR(500),
+      category VARCHAR(50),
+      default_price VARCHAR(10),
+      created_at TIMESTAMP,
+      updated_at TIMESTAMP
+    );`;
+
+execute(text).then(result => {
+  if (result) {
+    console.log('Table created');
+  }
+
+});
