4a06842c9ea868f4a5a0aeddbcab952aaa0ed7db
Tony Vo <vo.tony12@gmail.com>
2023-01-24 20:40:13 -0700

    cleaned up routers

--- server/controllers/initGetData.js
+++ server/controllers/initGetData.js
@@ -79,20 +79,6 @@ exports.getProductRelatedControl = (req, res) => {
     }
   })
 
-  // const options = {
-  //   method: 'GET',
-  //   url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${incomingParamProductId}/related`,
-  //   headers: { Authorization: process.env.AUTH_SECRET },
-  // };
-  // axios(options)
-  //   .then((result) => {
-  //     // Logic code controller need to import function
-  //     res.status(200).send(result.data)
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     res.status(500).send(err)
-  //   })
 };
 
 exports.getProductReviewsControl = (req, res) => {
